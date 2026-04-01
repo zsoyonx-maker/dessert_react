f024d2d82f38607d01a0fa73e7cf9df51ef3a46d
zsoyonx-maker <zsoyonx@gmail.com>
2026-04-01 17:28:43 +0900

    빌드완성(수정)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import Slider from "./components/Slider";
 import Image from "./components/Image";
 import ImageText from "./components/ImageText";
@@ -22,12 +23,14 @@ const App = () => {
     return (
         <>
             <Header />
-            <Slider title="슬라이더 유형" />
-            <Image title="이미지 유형 만들기" />
-            <ImageText title="이미지 텍스트 유형" color="red" />
-            <Card title="카페, 그 이상의 문화를 만들다" element="green" />
-            <Banner title="배너 영역" />
-            <Text title="다체로운 베이커리 컬렉션" />
+            <Main>
+                <Slider title="슬라이더 유형" />
+                <Image title="이미지 유형 만들기" />
+                <ImageText title="이미지 텍스트 유형" color="red" />
+                <Card title="카페, 그 이상의 문화를 만들다" element="green" />
+                <Banner title="배너 영역" />
+                <Text title="다체로운 베이커리 컬렉션" />
+            </Main>
             <Footer />
         </>
     );
